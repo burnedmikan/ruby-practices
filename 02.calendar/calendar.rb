466b984d040c2cf70c527165c29da9e4fde72158
burnedmikan <yakimikan25@gmail.com>
2026-05-24 23:25:57 +0900

    クラスで実装するように修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -6,6 +6,7 @@ require 'date'
 opt = OptionParser.new
 params = {}
 
+=begin
 def draw_calendar(year: , month:)
 
     japanese_wday_of_week = ["日","月","火","水","木","金","土"]
@@ -61,6 +62,111 @@ def draw_calendar(year: , month:)
 
     end
 end
+=end
+
+class Calendar
+
+    # 日本語の曜日表記はクラス変数としてもたせておく
+    @@japanese_wday_of_week = ["日","月","火","水","木","金","土"] 
+
+    def initialize(year: , month:)
+
+        @year = year
+        @month = month
+
+        # 月初の曜日
+        @first_wday_of_month = Date.new(year, month, 1).wday
+        # 月末の日にち
+        @end_day_of_month = Date.new(year, month, -1).day
+
+        # 月初の日曜日
+        @first_sunday_of_month = search_weekday(0)
+        # 月初の土曜日
+        @first_saturday_of_month = search_weekday(6)
+
+    end
+
+    # 指定された月初の曜日の日付を返すメソッド
+    def search_weekday(targetday)
+        search_day = Date.new(@year, @month, 1)
+        7.times do 
+            if search_day.wday == targetday
+                return search_day.day
+            end
+            search_day = search_day.next
+        end
+    end
+
+    # カレンダーのヘッダー部分描画
+    def draw_header()
+        
+        # 月数が二桁かどうかで、半角スペースの数を変えて描画
+        if @month < 10 
+            puts "\s" * 6 +  "#{@month}月 #{@year}"
+        else 
+            puts "\s" * 5 +  "#{@month}月 #{@year}"
+        end
+
+        # 曜日の日本語名を描画
+        puts @@japanese_wday_of_week.join("\s")
+
+    end
+
+    # 本体描画前の開始位置までのスペース埋め描画
+    def draw_pre_body()
+
+        # 月初までのスペース埋めの数の計算
+        first_count_of_space = case @first_wday_of_month
+        when 0,1
+            2 * @first_wday_of_month
+        else
+            2 + (@first_wday_of_month - 1) * 3
+        end
+
+        # 描画
+        print("\s" * first_count_of_space)
+    end
+
+    # 週初(日曜日)かどうかの判定。
+    def first_weekday?(day)
+        (day - @first_sunday_of_month) % 7 == 0 
+    end
+
+    # 週末(土曜日)かどうかの判定。
+    def weekend_day?(day)
+        (day - @first_saturday_of_month) % 7 == 0
+    end
+
+    # カレンダーの本体部分描画
+    def draw_body()
+        (1..@end_day_of_month).each do |day|
+            # 日付の前に必要となるスペースの数の管理用の変数
+            count_of_space = 0
+
+            # 週初(日曜日)かどうかの判定。日曜日以外であればスペースを追加
+            count_of_space += 1 unless first_weekday?(day)  
+
+            # 日付が1桁であれば、スペースを追加
+            count_of_space += 1 if day < 10
+
+            # 週末(土曜日)かどうかの判定。土曜日かつ最終日でなければ改行出力。それ以外であれば改行なしの出力
+            if weekend_day?(day) && day != @end_day_of_month
+                puts("\s" * count_of_space + day.to_s)
+            else
+                print("\s" * count_of_space + day.to_s)
+            end
+        end
+        #描画が終わったら最後に改行をいれておく
+        puts "" 
+    end
+
+
+    def draw_calendar()
+        draw_header
+        draw_pre_body
+        draw_body
+    end
+end
 
 # オプションのブロック登録
 opt.on('-y year') {|v|  v }
@@ -87,5 +193,6 @@ else
     params[:y] = Date.today.year
 end 
 
-# カレンダー描画
-draw_calendar(year: params[:y], month: params[:m])
+# カレンダーオブジェクトの用意と描画
+calendar = Calendar.new(year: params[:y], month: params[:m])
+calendar.draw_calendar
