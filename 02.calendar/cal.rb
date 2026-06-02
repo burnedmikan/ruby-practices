12d71d9de889e6ee03fc7bd65f96b447063f33bf
burnedmikan <yakimikan25@gmail.com>
2026-06-02 20:50:38 +0900

    レビュー指摘修正&rubyスタイルガイドに合わせた修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,68 +4,47 @@ require 'optparse'
 require 'date'
 
 class Calendar
-  def initialize(year: , month:)
+  def initialize(year, month)
     @start_date = Date.new(year, month, 1)
     @end_date = Date.new(year, month, -1)
   end
 
-  private
-
-  # カレンダーのヘッダー部分描画
-  def draw_header()
-    puts "#{@start_date.strftime('%B')} #{@start_date.strftime('%Y')}".center(20)
-    puts ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"] .join(" ")
-  end
-
-  # カレンダーの本体部分描画
-  def draw_body()
-    day_array = []
-
-    # 月初曜日までのスペース埋め
-    @start_date.wday.times { day_array.push("  ") }
-
-    @start_date.step(@end_date) do |target_date|
-      day_array.push(target_date.day.to_s.rjust(2))
-
-      if target_date.saturday? || target_date == @end_date
-        puts(day_array.join(" "))
-        day_array.clear
-      end
-    end
-  end
-
-  public
-  # カレンダー描画するメソッド
-  def draw_calendar()
+  def draw
     draw_header
     draw_body
+  end
+
+  private
+
+  def draw_header
+    puts @start_date.strftime('%B %Y').center(20)
+    puts "Su Mo Tu We Th Fr Sa"
+  end
+
+  def draw_body
+    days_array = Array.new(@start_date.wday, "  ")
+
+    @start_date.step(@end_date) do |target_date|
+      days_array.push(target_date.day.to_s.rjust(2))
+
+      if target_date.saturday? || target_date == @end_date
+        puts days_array.join(" ")
+        days_array.clear
+      end
+    end
   end
 end
 
 opt = OptionParser.new
 params = {}
 
-# オプションのブロック登録
-opt.on('-y year') {|v|  v }
+opt.on('-y year') {|v| v }
 opt.on('-m month') {|v| v }
 
-# オプションをparamsにパース
 opt.parse!(ARGV, into: params)
 
-# monthの入力チェックおよび変換と省略時の代入
-if params.has_key?(:m)
-  params[:m] = params[:m].to_i
-else 
-  params[:m] = Date.today.month
-end 
+params[:m] = params.has_key?(:m) ? params[:m].to_i : Date.today.month
+params[:y] = params.has_key?(:y) ? params[:y].to_i : Date.today.year
 
-# yearの入力チェックおよび変換と省略時の代入
-if params.has_key?(:y)
-  params[:y] = params[:y].to_i
-else 
-  params[:y] = Date.today.year
-end 
-
-# カレンダーインスタンスの用意と描画
-calendar = Calendar.new(year: params[:y], month: params[:m])
-calendar.draw_calendar
+calendar = Calendar.new(params[:y], params[:m])
+calendar.draw
